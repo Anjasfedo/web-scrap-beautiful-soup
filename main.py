a567cc74956c0a18aacad81233e7cbbc60493518
AnjasFedo <fedoaf98@gmail.com>
2024-01-17 09:00:25 +0700

    find & find_all Method

--- main.py
+++ main.py
@@ -2,5 +2,20 @@ from bs4 import BeautifulSoup
 
 with open("index.html", "r") as html_file:
     content = html_file.read()
-    print(content)
+    # print(content)
+    
+    soup = BeautifulSoup(content, "lxml")
+    
+    # print(soup.prettify())
+    
+    # Search first element that found
+    # tags = soup.find("h5")
+    
+    # Get all element
+    # tags = soup.find_all("h5")
+    
+    courses_html_tags = soup.find_all("h5")
+    
+    for course in courses_html_tags: 
+        print(course.text)
     
