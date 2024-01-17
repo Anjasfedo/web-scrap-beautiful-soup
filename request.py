91e2e443dea2a1e7a080f2e206fe9b141e5a8355
AnjasFedo <fedoaf98@gmail.com>
2024-01-17 09:36:58 +0700

    find_all on Requested HTML Element

--- request.py
+++ request.py
@@ -8,6 +8,19 @@ html_text = requests.get(URL).text
 
 soup = BeautifulSoup(html_text, "lxml")
 
-job = soup.find("li", class_="clearfix job-bx wht-shd-bx")
+jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
 
-print(job)
+for job in jobs:
+    company_name = job.find("h3", class_="joblist-comp-name").text.replace(" ", "")
+    
+    skills = job.find("span", class_="srp-skills").text.replace(" ", "")
+    
+    # print(company_name.text.replace(" ", ""))
+
+    # print(skills.text.replace(" ", ""))
+    
+    print(f'''
+          Company Name: {company_name}
+          Required Skills: {skills}
+          ''')
+
